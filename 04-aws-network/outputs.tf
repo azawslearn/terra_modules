73959c668f30ddb1ca86626e7ef63b9fcafafc10
azawslearn <134204024+azawslearn@users.noreply.github.com>
2024-02-06 09:06:58 +0200

    Update outputs.tf

--- 04-aws-network/outputs.tf
+++ 04-aws-network/outputs.tf
@@ -1,16 +1,16 @@
 output "vpc_id" {
   description = "ID of the created VPC"
-  value       = module.aws_vpc.vpc_id
+  value       = aws_vpc.my_vpc.id
 }
 
 output "public_subnet_ids" {
   description = "IDs of the public subnets"
-  value       = [aws_subnet.public_subnet[*].id]
+  value       = aws_subnet.public_subnet[*].id
 }
 
 output "private_subnet_ids" {
   description = "IDs of the private subnets"
-  value       = [aws_subnet.private_subnet[*].id]
+  value       = aws_subnet.private_subnet[*].id
 }
 
 output "security_group_id" {
